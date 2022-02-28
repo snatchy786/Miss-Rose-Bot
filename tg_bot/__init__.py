860f173e3f7cf0f77c1c3fc580fc6de694ec94b1
Bantilope <90923544+snatchy786@users.noreply.github.com>
2022-02-28 18:52:03 +0000

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -48,7 +48,7 @@ if ENV:
     PORT = int(os.environ.get('PORT', 5000))
     CERT_PATH = os.environ.get("CERT_PATH")
 
-    DB_URI = os.environ.get('DATABASE_URL')
+    DB_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
     DONATION_LINK = os.environ.get('DONATION_LINK')
     LOAD = os.environ.get("LOAD", "").split()
     NO_LOAD = os.environ.get("NO_LOAD", "").split()
